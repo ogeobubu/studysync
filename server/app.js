5c05939c77e515d82f9eacc4108eec3461718506
ogeobubu <ogeobubu@gmail.com>
2025-06-19 16:18:33 +0100

    recent changes

--- server/app.js
+++ server/app.js
@@ -55,4 +55,7 @@ const createApp = () => {
 const app = createApp();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
